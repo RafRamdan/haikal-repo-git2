f6958e0ba4fb45133bc71fd1d37a6655f4168f16
yourkal <haikalridha07@gmail.com>
2023-07-31 13:43:16 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello word!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis ex commodi consectetur aperiam dolores blanditiis veniam fugit, alias atque labore doloribus possimus numquam nobis itaque necessitatibus corporis modi dicta!</p>
 </body>
 </html>
