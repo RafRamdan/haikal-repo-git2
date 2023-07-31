fcb18017882757fcb24c6187f5a9c2b70649f368
yourkal <haikalridha07@gmail.com>
2023-07-31 14:05:12 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello word!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis ex commodi consectetur aperiam dolores blanditiis veniam fugit, alias atque labore doloribus possimus numquam nobis itaque necessitatibus corporis modi dicta!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
